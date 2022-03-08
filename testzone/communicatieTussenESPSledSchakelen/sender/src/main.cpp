9ef8079d796c2644ea5fcad452b866a28bc98150
RamautLyssa <lyssa.ramaut@student.kuleuven.be>
2022-03-08 21:08:32 +0100

    versie sender lyssa

--- testzone/communicatieTussenESPSledSchakelen/sender/src/main.cpp
+++ testzone/communicatieTussenESPSledSchakelen/sender/src/main.cpp
@@ -1,9 +1,118 @@
-#include <Arduino.h>
+#include "WiFi.h"
+#include "PubSubClient.h" //pio lib install "knolleary/PubSubClient"
 
-void setup() {
-  // put your setup code here, to run once:
+#define SSID "NETGEAR68"
+#define PWD "excitedtuba713"
+
+#define MQTT_SERVER "192.168.1.2"
+#define MQTT_PORT 1883
+
+#define LED_PIN 2
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void callback(char *topic, byte *message, unsigned int length);
+
+void setup_wifi()
+{
+  delay(10);
+  Serial.println("Connecting to WiFi..");
+
+  WiFi.begin(SSID, PWD);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+
+  Serial.begin(115200);
+
+  setup_wifi();
+  client.setServer(MQTT_SERVER, MQTT_PORT);
+  client.setCallback(callback);
+
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void callback(char *topic, byte *message, unsigned int length)
+{
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+}
+
+
+void reconnect()
+{
+  // Loop until we're reconnected
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    // creat unique client ID
+    // in Mosquitto broker enable anom. access
+    if (client.connect("Lyssa"))
+    {
+      Serial.println("connected");
+      // Subscribe
+      // client.subscribe("input/#");
+      //subscribe("TrappenMaar/status");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void loop()
+{
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 5000)
+  {
+    lastMsg = now;
+  }
+
+
+
+  client.publish("TrappenMaar", "1");
+  delay(3000);
+  client.publish("TrappenMaar", "0");
+  delay(3000);
+  client.publish("TrappenMaar", "2");
+  delay(3000);
+
 }
