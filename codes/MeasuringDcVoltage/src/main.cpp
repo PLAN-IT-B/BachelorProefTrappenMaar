2c905a30e17ae1069d2b4a4d82fc17fdf01e4950
RamautLyssa <lyssa.ramaut@student.kuleuven.be>
2022-03-21 14:58:03 +0100

    aanpassing buffer

--- codes/MeasuringDcVoltage/src/main.cpp
+++ codes/MeasuringDcVoltage/src/main.cpp
@@ -1,5 +1,43 @@
 #include <Adafruit_PN532.h>
 #include <LiquidCrystal_I2C.h>
+#include "WiFi.h"
+#include "PubSubClient.h"
+
+#define SSID "NETGEAR68"
+#define PWD "excitedtuba713"
+
+#define MQTT_SERVER "192.168.1.2"
+#define MQTT_PORT 1883
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+int level;
+
+String messageSend;
+
+void callback(char *topic, byte *message, unsigned int length);
+
+void setup_wifi()
+{
+  delay(10);
+  Serial.println("Connecting to WiFi..");
+
+  WiFi.begin(SSID, PWD);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
 
 LiquidCrystal_I2C lcd (0x27, 20,4);  //
 byte block[] = {
@@ -81,9 +119,12 @@ byte emptyBlock[] = {
 
 void setup() {
   // initialize serial communication at 9600 bits per second:
+
   lcd.init();
   lcd.backlight();
   Serial.begin(115200);                // Start serial monitor after a few seconds. Mainly for testing code to get it to work.
+  client.setServer(MQTT_SERVER, MQTT_PORT);
+  client.setCallback(callback);
   lcd.begin(20,4);
   
   lcd.createChar(0, block);
@@ -104,6 +145,53 @@ void setup() {
   lcd.setCursor(17,3); lcd.write(5);
  
 }
+void callback(char *topic, byte *message, unsigned int length)
+{
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)message[i]);
+    messageSend += (char)message[i];
+  }
+  Serial.println();
+  Serial.println(messageSend);
+
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+}
+
+void reconnect()
+{
+  // Loop until we're reconnected
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    // creat unique client ID
+    // in Mosquitto broker enable anom. access
+    if (client.connect("fiets"))
+    {
+      Serial.println("connected");
+      //hij luistert op trappen maar, fiets
+      //hij schrijft naar trappen maar, buffer
+      client.subscribe("TrappenMaar/fiets");
+      //subscribe("TrappenMaar/status");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+
 void display(int r) {
   lcd.setCursor(15, 2);
   if (int(r) < 10) {
@@ -111,11 +199,20 @@ void display(int r) {
   }
   lcd.print(r);
     if( (r)==0) {
+      //eerst dan even kijken of het nul blijft
+      //of fouten eruit te filteren
+      delay(250);
+      if((r)==0) {
       lcd.setCursor(0,0); lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);
       lcd.setCursor(0,1); lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);
       lcd.setCursor(0,2); lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);lcd.write(6);
         }
-    else if((0<(r)) && ((r) <=10)) {
+        else{
+          display(r);
+        }
+      level = 0;    
+    }
+    else if((0<(r)) && ((r) <=50)) {
       lcd.setCursor(0,0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);
       lcd.setCursor(0,1); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);
       lcd.setCursor(0,2); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);
@@ -123,9 +220,9 @@ void display(int r) {
       lcd.setCursor(5,1); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6);lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6);
       lcd.setCursor(5,2); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6);lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6);
 
-
+      level =1;
     }
-    else if((10 < (r)) && ((r) <=15)){
+    else if((50 < (r)) && ((r) <=150)){
       lcd.setCursor(0,0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);
       lcd.setCursor(0,1); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);
       lcd.setCursor(0,2); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);
@@ -133,8 +230,9 @@ void display(int r) {
       lcd.setCursor(10,1); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6);lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); 
       lcd.setCursor(10,2); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6);lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); 
 
+      level =2;
  }
-    else if((15 < (r)) && ((r) <=25)){
+    else if((150 < (r)) && ((r) <=250)){
       lcd.setCursor(0,0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);lcd.write(0);lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);
       lcd.setCursor(0,1); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);lcd.write(0);lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);
       lcd.setCursor(0,2); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);lcd.write(0);lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);
@@ -142,19 +240,21 @@ void display(int r) {
       lcd.setCursor(15,1); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6);
       lcd.setCursor(15,2); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6); lcd.write(6);
 
+      level = 3;
 }
 
-    else if((r)> 25) {
+    else if((r)> 250) {
       lcd.setCursor(0,0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);lcd.write(0);lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);
       lcd.setCursor(0,1); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);lcd.write(0);lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);
       lcd.setCursor(0,2); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);lcd.write(0);lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0); lcd.write(0);
-      
+
+      level = 4;   
 }
   
 }
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
+  // read the input on analog pin 34:
   int sensorValue = analogRead(34);
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
   float voltage = sensorValue * (3.3 / 4095.0);
@@ -163,8 +263,16 @@ void loop() {
   // print out the value you read:
   Serial.print(voltage);
   Serial.print(" ");
-  Serial.println(10*voltage);
-  display(10*voltage);
+  Serial.println(100*voltage);
+  display(100*voltage);
+  
+  char cstr[16];
+  if(messageSend == "Y"){
+    itoa(level, cstr,10);
+    client.publish("TrappenMaar/buffer", cstr);
+  }
+
+
 }
 
 
