1f165518bc88d4a250e5f7e984b8134181197274
VDSander <sander.vandriessche1@student.kuleuven.be>
2022-04-04 18:45:17 +0200

    kicad

---
+++ testzone/rgbNieuw/rgbNieuw/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+#define dutyPin 13
+#define yellow1 12
+#define yellow2 14
+#define yellow3 27
+#define green 26
+#define red 25
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(dutyPin, OUTPUT); pinMode(yellow1, OUTPUT); pinMode(yellow2, OUTPUT);
+  pinMode(yellow3, OUTPUT); pinMode(green, OUTPUT); pinMode(red, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
