f2db16118d041eb24d77dc3e7156856398b07691
VDSander <sander.vandriessche1@student.kuleuven.be>
2022-05-11 13:47:34 +0200

    testcode buffer
    kleuren oplopend

--- testzone/rgbNieuw/rgbNieuw/src/main.cpp
+++ testzone/rgbNieuw/rgbNieuw/src/main.cpp
@@ -48,123 +48,130 @@ void setup() {
 
 void loop() {
   
-  if(score==0){
+  if(score<interval){
   //niets
   digitalWrite(rood1, LOW);digitalWrite(rood2, LOW);digitalWrite(rood3, LOW); digitalWrite(rood4, LOW);digitalWrite(rood5, LOW);
   digitalWrite(yellow1, LOW);digitalWrite(yellow2, LOW);digitalWrite(yellow3, LOW); digitalWrite(yellow4, LOW);digitalWrite(yellow5, LOW);digitalWrite(yellow6, LOW);
   digitalWrite(groen1, LOW);digitalWrite(groen2, LOW);digitalWrite(groen3, LOW); digitalWrite(groen4, LOW);digitalWrite(groen5, LOW);
   ledcWrite(PWM_CHANNEL, dutyCycle);
   } 
-  else if((0*interval<(score)) && ((score) <=1*interval)) {
+  else if((1*interval<(score)) && ((score) <=2*interval)) {
   //rood1
   digitalWrite(rood1, HIGH);digitalWrite(rood2, LOW);digitalWrite(rood3, LOW); digitalWrite(rood4, LOW);digitalWrite(rood5, LOW);
   digitalWrite(yellow1, LOW);digitalWrite(yellow2, LOW);digitalWrite(yellow3, LOW); digitalWrite(yellow4, LOW);digitalWrite(yellow5, LOW);digitalWrite(yellow6, LOW);
   digitalWrite(groen1, LOW);digitalWrite(groen2, LOW);digitalWrite(groen3, LOW); digitalWrite(groen4, LOW);digitalWrite(groen5, LOW);
   ledcWrite(PWM_CHANNEL, dutyCycle);
   }
-  else if((1*interval<(score)) && ((score) <=2*interval)) {
+  else if((2*interval<(score)) && ((score) <=3*interval)) {
   //rood2
   digitalWrite(rood1, HIGH);digitalWrite(rood2, HIGH);digitalWrite(rood3, LOW); digitalWrite(rood4, LOW);digitalWrite(rood5, LOW);
   digitalWrite(yellow1, LOW);digitalWrite(yellow2, LOW);digitalWrite(yellow3, LOW); digitalWrite(yellow4, LOW);digitalWrite(yellow5, LOW);digitalWrite(yellow6, LOW);
   digitalWrite(groen1, LOW);digitalWrite(groen2, LOW);digitalWrite(groen3, LOW); digitalWrite(groen4, LOW);digitalWrite(groen5, LOW);
   ledcWrite(PWM_CHANNEL, dutyCycle);
   }
-  else if((2*interval<(score)) && ((score) <=3*interval)) {
+  else if((3*interval<(score)) && ((score) <=4*interval)) {
   //rood3
   digitalWrite(rood1, HIGH);digitalWrite(rood2, HIGH);digitalWrite(rood3, HIGH); digitalWrite(rood4, LOW);digitalWrite(rood5, LOW);
   digitalWrite(yellow1, LOW);digitalWrite(yellow2, LOW);digitalWrite(yellow3, LOW); digitalWrite(yellow4, LOW);digitalWrite(yellow5, LOW);digitalWrite(yellow6, LOW);
   digitalWrite(groen1, LOW);digitalWrite(groen2, LOW);digitalWrite(groen3, LOW); digitalWrite(groen4, LOW);digitalWrite(groen5, LOW);
   ledcWrite(PWM_CHANNEL, dutyCycle);
   }
-  else if((3*interval<(score)) && ((score) <=4*interval)) {
+  else if((4*interval<(score)) && ((score) <=5*interval)) {
  //rood4
   digitalWrite(rood1, HIGH);digitalWrite(rood2, HIGH);digitalWrite(rood3, HIGH); digitalWrite(rood4, HIGH);digitalWrite(rood5, LOW);
   digitalWrite(yellow1, LOW);digitalWrite(yellow2, LOW);digitalWrite(yellow3, LOW); digitalWrite(yellow4, LOW);digitalWrite(yellow5, LOW);digitalWrite(yellow6, LOW);
   digitalWrite(groen1, LOW);digitalWrite(groen2, LOW);digitalWrite(groen3, LOW); digitalWrite(groen4, LOW);digitalWrite(groen5, LOW);
   ledcWrite(PWM_CHANNEL, dutyCycle);
   }
-  else if((4*interval<(score)) && ((score) <=5*interval)) {
+  else if((5*interval<(score)) && ((score) <=6*interval)) {
   //rood5
   digitalWrite(rood1, HIGH);digitalWrite(rood2, HIGH);digitalWrite(rood3, HIGH); digitalWrite(rood4, HIGH);digitalWrite(rood5, HIGH);
   digitalWrite(yellow1, LOW);digitalWrite(yellow2, LOW);digitalWrite(yellow3, LOW); digitalWrite(yellow4, LOW);digitalWrite(yellow5, LOW);digitalWrite(yellow6, LOW);
   digitalWrite(groen1, LOW);digitalWrite(groen2, LOW);digitalWrite(groen3, LOW); digitalWrite(groen4, LOW);digitalWrite(groen5, LOW);
   ledcWrite(PWM_CHANNEL, dutyCycle);
   }
-  else if((5*interval<(score)) && ((score) <=6*interval)) {
+  else if((6*interval<(score)) && ((score) <=7*interval)) {
   //oranje1
   digitalWrite(rood1, HIGH);digitalWrite(rood2, HIGH);digitalWrite(rood3, HIGH); digitalWrite(rood4, HIGH);digitalWrite(rood5, HIGH);
   digitalWrite(yellow1, HIGH);digitalWrite(yellow2, LOW);digitalWrite(yellow3, LOW); digitalWrite(yellow4, LOW);digitalWrite(yellow5, LOW);digitalWrite(yellow6, LOW);
   digitalWrite(groen1, LOW);digitalWrite(groen2, LOW);digitalWrite(groen3, LOW); digitalWrite(groen4, LOW);digitalWrite(groen5, LOW);
   ledcWrite(PWM_CHANNEL, dutyCycle);
   }
-  else if((6*interval<(score)) && ((score) <=7*interval)) {
+  else if((7*interval<(score)) && ((score) <=8*interval)) {
   //oranje2
   digitalWrite(rood1, HIGH);digitalWrite(rood2, HIGH);digitalWrite(rood3, HIGH); digitalWrite(rood4, HIGH);digitalWrite(rood5, HIGH);
   digitalWrite(yellow1, HIGH);digitalWrite(yellow2, HIGH);digitalWrite(yellow3, LOW); digitalWrite(yellow4, LOW);digitalWrite(yellow5, LOW);digitalWrite(yellow6, LOW);
   digitalWrite(groen1, LOW);digitalWrite(groen2, LOW);digitalWrite(groen3, LOW); digitalWrite(groen4, LOW);digitalWrite(groen5, LOW);
   ledcWrite(PWM_CHANNEL, dutyCycle);
   }
-  else if((7*interval<(score)) && ((score) <=8*interval)) {
+  else if((8*interval<(score)) && ((score) <=9*interval)) {
   //oranje3
   digitalWrite(rood1, HIGH);digitalWrite(rood2, HIGH);digitalWrite(rood3, HIGH); digitalWrite(rood4, HIGH);digitalWrite(rood5, HIGH);
   digitalWrite(yellow1, HIGH);digitalWrite(yellow2, HIGH);digitalWrite(yellow3, HIGH); digitalWrite(yellow4, LOW);digitalWrite(yellow5, LOW);digitalWrite(yellow6, LOW);
   digitalWrite(groen1, LOW);digitalWrite(groen2, LOW);digitalWrite(groen3, LOW); digitalWrite(groen4, LOW);digitalWrite(groen5, LOW);
   ledcWrite(PWM_CHANNEL, dutyCycle);
   }
-  else if((8*interval<(score)) && ((score) <=9*interval)) {
+  else if((9*interval<(score)) && ((score) <=10*interval)) {
   //oranje4
   digitalWrite(rood1, HIGH);digitalWrite(rood2, HIGH);digitalWrite(rood3, HIGH); digitalWrite(rood4, HIGH);digitalWrite(rood5, HIGH);
   digitalWrite(yellow1, HIGH);digitalWrite(yellow2, HIGH);digitalWrite(yellow3, HIGH); digitalWrite(yellow4, HIGH);digitalWrite(yellow5, LOW);digitalWrite(yellow6, LOW);
   digitalWrite(groen1, LOW);digitalWrite(groen2, LOW);digitalWrite(groen3, LOW); digitalWrite(groen4, LOW);digitalWrite(groen5, LOW);
   ledcWrite(PWM_CHANNEL, dutyCycle);
   }
-  else if((9*interval<(score)) && ((score) <=10*interval)) {
+  else if((10*interval<(score)) && ((score) <=11*interval)) {
   //oranje5
   digitalWrite(rood1, HIGH);digitalWrite(rood2, HIGH);digitalWrite(rood3, HIGH); digitalWrite(rood4, HIGH);digitalWrite(rood5, HIGH);
   digitalWrite(yellow1, HIGH);digitalWrite(yellow2, HIGH);digitalWrite(yellow3, HIGH); digitalWrite(yellow4, HIGH);digitalWrite(yellow5, HIGH);digitalWrite(yellow6, LOW);
   digitalWrite(groen1, LOW);digitalWrite(groen2, LOW);digitalWrite(groen3, LOW); digitalWrite(groen4, LOW);digitalWrite(groen5, LOW);
   ledcWrite(PWM_CHANNEL, dutyCycle);
   }
-  else if((10*interval<(score)) && ((score) <=11*interval)) {
+  else if((11*interval<(score)) && ((score) <=12*interval)) {
   //oranje6
   digitalWrite(rood1, HIGH);digitalWrite(rood2, HIGH);digitalWrite(rood3, HIGH); digitalWrite(rood4, HIGH);digitalWrite(rood5, HIGH);
   digitalWrite(yellow1, HIGH);digitalWrite(yellow2, HIGH);digitalWrite(yellow3, HIGH); digitalWrite(yellow4, HIGH);digitalWrite(yellow5, HIGH);digitalWrite(yellow6, HIGH);
   digitalWrite(groen1, LOW);digitalWrite(groen2, LOW);digitalWrite(groen3, LOW); digitalWrite(groen4, LOW);digitalWrite(groen5, LOW);
   ledcWrite(PWM_CHANNEL, dutyCycle);
   }
-  else if((11*interval<(score)) && ((score) <=12*interval)) {
+  else if((12*interval<(score)) && ((score) <=13*interval)) {
   //groen1
   digitalWrite(rood1, HIGH);digitalWrite(rood2, HIGH);digitalWrite(rood3, HIGH); digitalWrite(rood4, HIGH);digitalWrite(rood5, HIGH);
   digitalWrite(yellow1, HIGH);digitalWrite(yellow2, HIGH);digitalWrite(yellow3, HIGH); digitalWrite(yellow4, HIGH);digitalWrite(yellow5, HIGH);digitalWrite(yellow6, HIGH);
   digitalWrite(groen1, HIGH);digitalWrite(groen2, LOW);digitalWrite(groen3, LOW); digitalWrite(groen4, LOW);digitalWrite(groen5, LOW);
   ledcWrite(PWM_CHANNEL, dutyCycle);
   }
-  else if((12*interval<(score)) && ((score) <=13*interval)) {
+  else if((13*interval<(score)) && ((score) <=14*interval)) {
   //groen2
   digitalWrite(rood1, HIGH);digitalWrite(rood2, HIGH);digitalWrite(rood3, HIGH); digitalWrite(rood4, HIGH);digitalWrite(rood5, HIGH);
   digitalWrite(yellow1, HIGH);digitalWrite(yellow2, HIGH);digitalWrite(yellow3, HIGH); digitalWrite(yellow4, HIGH);digitalWrite(yellow5, HIGH);digitalWrite(yellow6, HIGH);
   digitalWrite(groen1, HIGH);digitalWrite(groen2, HIGH);digitalWrite(groen3, LOW); digitalWrite(groen4, LOW);digitalWrite(groen5, LOW);
   ledcWrite(PWM_CHANNEL, dutyCycle);
   }
-  else if((13*interval<(score)) && ((score) <=14*interval)) {
+  else if((14*interval<(score)) && ((score) <=15*interval)) {
   //groen3
   digitalWrite(rood1, HIGH);digitalWrite(rood2, HIGH);digitalWrite(rood3, HIGH); digitalWrite(rood4, HIGH);digitalWrite(rood5, HIGH);
   digitalWrite(yellow1, HIGH);digitalWrite(yellow2, HIGH);digitalWrite(yellow3, HIGH); digitalWrite(yellow4, HIGH);digitalWrite(yellow5, HIGH);digitalWrite(yellow6, HIGH);
   digitalWrite(groen1, HIGH);digitalWrite(groen2, HIGH);digitalWrite(groen3, HIGH); digitalWrite(groen4, LOW);digitalWrite(groen5, LOW);
   ledcWrite(PWM_CHANNEL, dutyCycle);
   }
-  else if((14*interval<(score)) && ((score) <=15*interval)) {
+  else if((15*interval<(score)) && ((score) <=16*interval)) {
   //groen4
   digitalWrite(rood1, HIGH);digitalWrite(rood2, HIGH);digitalWrite(rood3, HIGH); digitalWrite(rood4, HIGH);digitalWrite(rood5, HIGH);
   digitalWrite(yellow1, HIGH);digitalWrite(yellow2, HIGH);digitalWrite(yellow3, HIGH); digitalWrite(yellow4, HIGH);digitalWrite(yellow5, HIGH);digitalWrite(yellow6, HIGH);
   digitalWrite(groen1, HIGH);digitalWrite(groen2, HIGH);digitalWrite(groen3, HIGH); digitalWrite(groen4, HIGH);digitalWrite(groen5, LOW);
   ledcWrite(PWM_CHANNEL, dutyCycle);
   }
-  else if((15*interval<(score)) && ((score) <=16*interval)) {
+  else if((16*interval<(score)) && ((score) <=17*interval)) {
   //groen5
   digitalWrite(rood1, HIGH);digitalWrite(rood2, HIGH);digitalWrite(rood3, HIGH); digitalWrite(rood4, HIGH);digitalWrite(rood5, HIGH);
   digitalWrite(yellow1, HIGH);digitalWrite(yellow2, HIGH);digitalWrite(yellow3, HIGH); digitalWrite(yellow4, HIGH);digitalWrite(yellow5, HIGH);digitalWrite(yellow6, HIGH);
   digitalWrite(groen1, HIGH);digitalWrite(groen2, HIGH);digitalWrite(groen3, HIGH); digitalWrite(groen4, HIGH);digitalWrite(groen5, HIGH);
   ledcWrite(PWM_CHANNEL, dutyCycle);
   }
+
+  if(score == 17*interval){
+    score = 0;
+  }
+  else score++;
+
+  delay(200);
 }
